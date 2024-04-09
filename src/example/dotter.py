741891d4c33bea2d9362a278ce684a97d7d698a2
Kuglatec <95873978+kuglatec@users.noreply.github.com>
2024-04-09 21:02:18 +0200

    Add files via upload

---
+++ src/example/dotter.py
@@ -0,0 +1,17 @@
+#Dotter file for an i3wm setup 
+Name = 'i3test'
+#dependencies
+
+dependencies = ["xorg", "i3", "polybar", "xorg"]
+
+configs = {
+  'i3': {
+    'file': ".config/i3/config",
+    'pkgdest': '.config/i3/config'
+  },
+
+  'polybar': {
+    'file': ".config/polybar/config",
+    'pkgdest': '.config/polybar/config'
+  }
+}
