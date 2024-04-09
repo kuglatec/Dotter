38f3418e0e998dc9aab992ce1afb826fed46590a
Kuglatec <95873978+kuglatec@users.noreply.github.com>
2024-04-09 21:08:38 +0200

    Update dotter.py

--- example/dotter.py
+++ example/dotter.py
@@ -6,12 +6,12 @@ dependencies = ["xorg", "i3", "polybar", "xorg"]
 
 configs = {
   'i3': {
-    'file': ".config/i3/config",
+    'file': "config/i3/config",
     'pkgdest': '.config/i3/config'
   },
 
   'polybar': {
-    'file': ".config/polybar/config",
+    'file': "config/polybar/config",
     'pkgdest': '.config/polybar/config'
   }
 }
