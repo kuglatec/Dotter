1d4fb7f61d2c9d1ecb9e65406c08ca0bc8d49bc7
Kuglatec <95873978+kuglatec@users.noreply.github.com>
2024-07-29 13:11:56 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 echo "Thank you for installing dotter!"
+echo "cloning repo..."
+git clone https://github.com/kuglatec/dotter
+cd dotter
 echo "Copying file..."
 chmod +x src/main.py
 cp src/main.py /bin/dotter
