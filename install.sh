79027dffe0abaf18bf9b28db39343c4b598e102f
Kuglatec <95873978+kuglatec@users.noreply.github.com>
2024-07-29 13:17:13 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@ echo "cloning repo..."
 git clone https://github.com/kuglatec/dotter
 cd dotter
 echo "Copying file..."
-chmod +x src/main.py
-cp src/main.py /bin/dotter
+sudo chmod +x src/main.py
+sudo cp src/main.py /bin/dotter
 echo "setup finished! start setting up you first package"
